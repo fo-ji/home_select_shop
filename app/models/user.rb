c2341db0eea57147fcfc5b478a48b95e0921b566
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-06-16 11:12:22 +0900

    バリデーション(address)の修正#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 7 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
-  validates :height, :body_weight, presence: true, length: { in: 2..3 }, format: { with: /^[0-9]+$/ }
+  validates :height, :body_weight, presence: true, length: { in: 2..3 }, format: { with: /\A[0-9]+\z/ }
   validates :foot_size, presence: true
 end
