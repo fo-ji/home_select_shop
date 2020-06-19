1513b60335d35367d11dba516254756c7a8f4bce
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-06-19 15:09:47 +0900

    FactoryBotの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"ヤマ"}
+    email                 {"aaaa@gmail.com"}
+    password              {"aaaa111"}
+    password_confirmation {"aaaa111"}
+    first_name            {"太郎"}
+    last_name             {"山田"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"ヤマダ"}
+    birthday              {"2000/01/01"}
+    height                {"160"}
+    body_weight           {"50"}
+    foot_size             {"1"}
   end
 end
