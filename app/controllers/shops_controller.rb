b05b630fbf5724fcc288e6768c3f5cd8ebfc43f9
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-06-20 15:04:59 +0900

    controller_ショップの実装

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,21 @@
+class ShopsController < ApplicationController
+  def new
+    @shop = Shop.new
+    @shop.users << current_user
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :email, :avatar, :introduction, :postal_code, :prefecture, :city, :address, :apartment, :phone_number , user_ids: [])
+  end
+end
