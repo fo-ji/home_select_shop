5f102bc2ea7aa4b0cc2d82795a8126f98c450e30
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-05 00:15:14 +0900

    brandモデルの単体テスト

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it "全て入力されていればOK" do
+      brand = create(:brand)
+      expect(brand).to be_valid
+    end
+    it "brandが空ならNG" do
+      brand = build(:brand, brand: nil)
+      brand.valid?
+      expect(brand.errors[:brand]).to include("を入力してください")
+    end
+  end
 end
