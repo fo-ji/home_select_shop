ab889a3712162adf2a99bf2894a2f815ef89202c
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-08 22:00:54 +0900

    モデル、コントローラーのつながりを分かりやすくする為、card→credit_cardコントローラーへ変更

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,9 +1,9 @@
-class CardController < ApplicationController
+class CreditCardsController < ApplicationController
   require "payjp"
 
   def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    credit_card = CreditCard.where(user_id: current_user.id)
+    redirect_to action: "show" if credit_card.exists?
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
@@ -17,8 +17,8 @@ class CardController < ApplicationController
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
+      @credit_card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @credit_card.save
         redirect_to action: "show"
       else
         redirect_to action: "pay"
@@ -27,25 +27,25 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    if credit_card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
       customer.delete
-      card.delete
+      credit_card.delete
     end
       redirect_to action: "new"
   end
 
   def show
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    if credit_card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
+      @default_card_information = customer.credit_cards.retrieve(credit_card.credit_card_id)
     end
   end
 end
--- spec/requests/credit_cards_request_spec.rb
+++ spec/requests/credit_cards_request_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Cards", type: :request do
+RSpec.describe "CreditCards", type: :request do
 
   describe "GET /new" do
     it "returns http success" do
-      get "/card/new"
+      get "/credit_cards/new"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/card/show"
+      get "/credit_cards/show"
       expect(response).to have_http_status(:success)
     end
   end
