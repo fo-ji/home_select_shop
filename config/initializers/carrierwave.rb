800092fbc5ec8c2fc449c8bcc2cba79f114e09e4
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-15 16:48:42 +0900

    S3設定の修正#3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,10 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
     config.storage :fog
+    config.cache_storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'homeselectshop'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/homeselectshop"
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
