20f2c53eaa303317578298ddec300ee31a6b8323
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-16 23:38:43 +0900

    テストを通す為、shopのfactoriesの追加

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :shop do
-    name         {"ヤマ"}
-    email        {"bbb@gmail.com"}
-    postal_code  {"222-2222"}
-    prefecture   {"大阪府"}
-    city         {"大阪市"}
-    address      {"北区1-1-1"}
-    apartment    {"丸ビル101"}
-    phone_number {"0611111111"}
+    name              {"ヤマ"}
+    email             {"bbb@gmail.com"}
+    postal_code       {"222-2222"}
+    prefecture        {"大阪府"}
+    city              {"大阪市"}
+    address           {"北区1-1-1"}
+    apartment         {"丸ビル101"}
+    phone_number      {"0611111111"}
+    official_site     {"https://www.sample.com/1"}
+    facebook_account  {"https://www.sample.com/2"}
+    line_account      {"https://www.sample.com/3"}
+    instagram_account {"https://www.sample.com/4"}
 
     trait :stylist do
       after(:build) do |shop|
