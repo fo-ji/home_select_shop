bbb4788604c8a45fcdb43e2c986c36de6f26f7f3
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-19 21:43:41 +0900

    レコメンド登録機能の実装

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -3,8 +3,9 @@ class Recommend < ApplicationRecord
   belongs_to :user
 
   validates :name, :image, :genre, :introduction, presence: true
+  validates :url, format: { with: /\A#{URI::regexp(%w(http https))}\z/ }
 
-  enum gender: { "雑貨": 0, "インテリア": 1, "音楽": 2, "本": 3, "その他": 4 }
+  enum genre: { "雑貨": 0, "インテリア": 1, "音楽": 2, "本": 3, "その他": 4 }
 
   mount_uploader :image, ImageUploader
 end
