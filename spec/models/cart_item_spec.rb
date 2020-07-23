b6859c8c25a2e48f25a0b54f34d330fd166efef6
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-23 23:53:31 +0900

    cart_itemモデルの単体テスト

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it "全て入力されていればOK" do
+      cart_item = build(:cart_item)
+      expect(cart_item).to be_valid
+    end
+    it "item_idが空ならNG" do
+      cart_item = build(:cart_item, item: nil)
+      cart_item.valid?
+      expect(cart_item.errors[:item]).to include("を入力してください")
+    end
+    it "cart_idが空ならNG" do
+      cart_item = build(:cart_item, cart: nil)
+      cart_item.valid?
+      expect(cart_item.errors[:cart]).to include("を入力してください")
+    end
+  end
 end
--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Talk, type: :model do
-  it "全て入力されていればOK" do
-    talk = build(:talk)
-    expect(talk).to be_valid
-  end
-  it "talkが空でもOK" do
-    talk = build(:talk, talk: nil)
-    expect(talk).to be_valid
-  end
-  it "imageが空でもOK" do
-    talk = build(:talk, image: nil)
-    expect(talk).to be_valid
-  end
-  it "community_idが空ならNG" do
-    talk = build(:talk, community: nil)
-    talk.valid?
-    expect(talk.errors[:community]).to include("を入力してください")
-  end
-  it "user_idが空ならNG" do
-    talk = build(:talk, user: nil)
-    talk.valid?
-    expect(talk.errors[:user]).to include("を入力してください")
+  describe '#create' do
+    it "全て入力されていればOK" do
+      talk = build(:talk)
+      expect(talk).to be_valid
+    end
+    it "talkが空でもOK" do
+      talk = build(:talk, talk: nil)
+      expect(talk).to be_valid
+    end
+    it "imageが空でもOK" do
+      talk = build(:talk, image: nil)
+      expect(talk).to be_valid
+    end
+    it "community_idが空ならNG" do
+      talk = build(:talk, community: nil)
+      talk.valid?
+      expect(talk.errors[:community]).to include("を入力してください")
+    end
+    it "user_idが空ならNG" do
+      talk = build(:talk, user: nil)
+      talk.valid?
+      expect(talk.errors[:user]).to include("を入力してください")
+    end
   end
 end
