44df8933046908df494ee608effce336b1eaffb3
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-23 23:11:54 +0900

    itemモデルテストの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,10 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images
   accepts_nested_attributes_for :brand
 
-  validates :name, :explain, :size, :postage, :shipping_date, :gender, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :explain, :size, :postage, :shipping_date, :gender, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 9999999}
-  validates :stock, presence: true, presence: true, length: { in: 1..3 }, format: { with: /\A[0-9]+\z/ }
+  validates :stock, presence: true, length: { in: 1..3 }, format: { with: /\A[0-9]+\z/ }
 
   enum size: { "XXS以下": 1, "XS(SS)": 2, "S": 3, "M": 4, "L": 5, "XL(LL)": 6, "2XL(3L)": 7, "3XL(4L)": 8, "4XL(5L)以上": 9, "FREESIZE": 10 }
   enum postage: { "送料無料": 1, "送料500円": 2, "送料1,000円": 3 }
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     postage       {"送料無料"}
     shipping_date {"4〜7日で発送"}
     gender        {"メンズ"}
+    stock         {"1"}
     user
     shop
     brand
