c8fc90550806bd1c7ea9a081ce891f52897394ac
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-07-29 21:57:03 +0900

    cart_coordinateモデルの実装

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,20 @@
 class Cart < ApplicationRecord
   has_many :cart_items
+  has_many :cart_coordinates
 
   def total_price
     cart_items.to_a.sum { |item| item.total_price }
   end
 
+  def coordinate_total_price
+    cart_coordinates.to_a.sum { |coordinate| coordinate.coordinate_total_price }
+  end
+
   def total_quantity
     cart_items.to_a.sum { |item| item.quantity }
+  end
+
+  def coordinate_total_quantity
+    cart_coordinates.to_a.sum { |coordinate| coordinate.quantity }
   end
 end
